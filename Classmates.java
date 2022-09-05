b67f1073c0f29a11556e3bc30be1599fd98d3df4
MehmetFath <112849596+MehmetFath@users.noreply.github.com>
2022-09-05 23:07:31 +0300

    Add files via upload

---
+++ Classmates.java
@@ -0,0 +1,11 @@
+package day02_JavaPractise;
+
+public class Classmates {
+    public static void main(String[] args) {
+        System.out.println("Mehmet");
+        System.out.println("Halil");
+        System.out.println("Gizem");
+        System.out.println("Deniz");
+        System.out.println("Seda");
+    }
+}
