c0d28d8101b1d50ba91a9c82adfd855dbad3df3b
MehmetFath <112849596+MehmetFath@users.noreply.github.com>
2022-09-06 00:14:21 +0300

    Add files via upload

---
+++ TryingSomething.java
@@ -0,0 +1,56 @@
+package day02_JavaPractise;
+
+public class TryingSomething {
+    public static void main(String[] args) {
+
+        System.out.println("CCCCCCCCCCCCC");
+        System.out.println("CC");
+        System.out.println("CC");
+        System.out.println("CC");
+        System.out.println("CC");
+        System.out.println("CC");
+        System.out.println("CCCCCCCCCCCCCC");
+        System.out.println();
+        System.out.println("CC          CC");
+        System.out.println(" CC        CC");
+        System.out.println("  CC      CC");
+        System.out.println("   CC    CC");
+        System.out.println("    CC  CC");
+        System.out.println("      CC");
+        System.out.println("      CC");
+        System.out.println("      CC");
+        System.out.println("      CC");
+        System.out.println();
+        System.out.println("CCCCCCCCCCCCCC");
+        System.out.println("CC           CC");
+        System.out.println("CC           CC");
+        System.out.println("CC           CC");
+        System.out.println("CC           CC");
+        System.out.println("CC           CC");
+        System.out.println("CC           CC");
+        System.out.println("CCCCCCCCCCCCCC");
+        System.out.println();
+        System.out.println("CCCCCCCCCCCCCCCC");
+        System.out.println("CC             C");
+        System.out.println("CC");
+        System.out.println("CCCCCCCCCCC");
+        System.out.println("CC");
+        System.out.println("CC             C");
+        System.out.println("CCCCCCCCCCCCCCCC");
+        System.out.println();
+        System.out.println("   CCCCCCCCCCC  ");
+        System.out.println("CC            CC");
+        System.out.println("CC            CC");
+        System.out.println("CC            CC");
+        System.out.println("CC            CC");
+        System.out.println("CC            CC");
+        System.out.println("   CCCCCCCCCCC  ");
+
+
+
+
+
+
+
+    }
+}
